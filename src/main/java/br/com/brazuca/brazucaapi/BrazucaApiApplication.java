afd31308c2e24ec4aaffc289f6beb1f972adf45e
unknown <alexandre@NOTSTEC18.sefaz.se.gov.br>
2022-07-15 20:14:04 -0300

    primeiro commit

---
+++ src/main/java/br/com/brazuca/brazucaapi/BrazucaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.brazuca.brazucaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazucaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazucaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brazuca/brazucaapi/BrazucaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brazuca.brazucaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazucaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
