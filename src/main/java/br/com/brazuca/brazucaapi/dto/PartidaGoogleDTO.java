aac3ebcf8d4baa1e2616be7e9a750c3a524f1f4e
unknown <alexandre@NOTSTEC18.sefaz.se.gov.br>
2022-07-15 20:58:59 -0300

    21. Criando o DTO base para as buscas

---
+++ src/main/java/br/com/brazuca/brazucaapi/dto/PartidaGoogleDTO.java
@@ -0,0 +1,35 @@
+package br.com.brazuca.brazucaapi.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PartidaGoogleDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	private String tempoPartida;
+	// Informações da equipe da Casa
+	private String nomeEquipeCasa;
+	private String urlLogoEquipeCasa;
+	private Integer placarEquipeCasa;
+	private String golsEquipeCasa;
+	private String placarEstendidoEquipeCasa;
+	// Informações da equipe da Visitante
+	private String nomeEquipeVisitante;
+	private String urlLogoEquipeVisitante;
+	private Integer placarEquipeVisitante;
+	private String golsEquipeVisitante;
+	private String placarEstendidoEquipeVisitante;
+}
---
+++ src/main/java/br/com/brazuca/brazucaapi/util/StatusPartida.java
@@ -0,0 +1,10 @@
+package br.com.brazuca.brazucaapi.util;
+
+public enum StatusPartida {
+
+	PARTIDA_NAO_INICIADA,
+	PARTIDA_EM_ENDAMENTO,
+	PARTIDA_ENCERRADA,
+	PARTIDA_PENALTIS;
+	
+}
